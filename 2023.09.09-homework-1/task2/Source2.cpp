63323abddce03d7f3f3d93f86388a148d549586a
st117165 <144274525+st117165@users.noreply.github.com>
2023-10-09 19:05:00 +0300

    Delete 2023.09.09-homework-1 directory

--- 2023.09.09-homework-1/task2/Source2.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-int main() {
-	int a = 0;
-	std::cin >> a;
-	std::cout << (a + 1) * a / 2;
-	return EXIT_SUCCESS;
-}
--- 2023.09.09-homework-1/task7/Source7.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-int main() {
-	int a = 0;
-	std::cin >> a;
-	std::cout << "The next number for the number " << a << " is " << a + 1 << "." << std::endl << "The previous number for the number " << a << " is " << a - 1 << ".";
-	return EXIT_SUCCESS;
-}
