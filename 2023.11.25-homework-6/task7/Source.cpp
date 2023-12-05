29d62ed0de1871ea9e3de7a05178d87f01ba2150
st117165 <st117165@student.spbu.ru>
2023-12-05 21:42:08 +0300

    Homerwork 6 tasks 6 and 7 reupload

--- 2023.11.25-homework-6/task7/Source.cpp
+++ 2023.11.25-homework-6/task7/Source.cpp
@@ -4,35 +4,35 @@ void hanoi(int n, int x, int y);
 
 int main(int argc, char* argv[])
 {
-	int n = 0;
-	std::cin >> n;
-	int a = 0;
-	int c = 0;
-	for (int i = 0; i < n; ++i)
-	{
-		std::cin >> a;
-		if (a > c)
-		{
-			hanoi(1, 1, 3);
-		}
-		else
-		{
-			hanoi(1, 1, 2);
-		}
-		c = a;
-	}
+    int n = 0;
+    std::cin >> n;
+    int a = 0;
+    int c = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> a;
+        if (a > c)
+        {
+            hanoi(1, 1, 3);
+        }
+        else
+        {
+            hanoi(1, 1, 2);
+        }
+        c = a;
+    }
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 void hanoi(int n, int x, int y)
 {
-	if (n > 0)
-	{
-		int res = 6 - x - y;
-		hanoi(n - 1, x, res);
-		std::cout << 0 << " " << x << " " << y << std::endl;
-		hanoi(n - 1, res, y);
-		std::cout << n << " " << 6 - x - y << " " << 3 << std::endl;
-	}
+    if (n > 0)
+    {
+        int res = 6 - x - y;
+        hanoi(n - 1, x, res);
+        std::cout << 0 << " " << x << " " << y << std::endl;
+        hanoi(n - 1, res, y);
+        std::cout << n << " " << 6 - x - y << " " << 3 << std::endl;
+    }
 }
