28eb97c7e10a8f225416e8c9ae90aadbfb3c5458
st117165 <st117165@student.spbu.ru>
2023-12-05 21:37:40 +0300

    Homework 6 task 6 and 7 reupload

--- 2023.11.25-homework-6/task6/Source.cpp
+++ 2023.11.25-homework-6/task6/Source.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-void recursion(int n, int from, int to);
+void recursion(int n, int x, int y);
 
 int main(int argc, char* argv[])
 {
@@ -11,21 +11,21 @@ int main(int argc, char* argv[])
 	return EXIT_SUCCESS;
 }
 
-void recursion(int n, int from, int to)
+void recursion(int n, int x, int y)
 {
 	if (n > 0)
 	{
-		int res = 6 - from - to;
-		recursion(n - 1, from, res);
+		int res = 6 - x - y;
+		recursion(n - 1, x, res);
 		if (n != 1)
 		{
-			std::cout << 0 << " " << from << " " << to << std::endl;
+			std::cout << 0 << " " << x << " " << y << std::endl;
 
 		}
 		else
 		{
-			std::cout << n << " " << 6 - from - to << " " << 3 << std::endl;
+			std::cout << n << " " << 6 - x - y << " " << 3 << std::endl;
 		}
-		recursion(n - 1, res, to);
+		recursion(n - 1, res, y);
 	}
 }
--- 2023.11.25-homework-6/task7/Source.cpp
+++ 2023.11.25-homework-6/task7/Source.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
-void hanoi(int n, int from, int to);
+void hanoi(int n, int x, int y);
 
 int main(int argc, char* argv[])
 {
 	int n = 0;
 	std::cin >> n;
 	int a = 0;
-	int temp = 0;
+	int c = 0;
 	for (int i = 0; i < n; ++i)
 	{
 		std::cin >> a;
-		if (a > temp)
+		if (a > c)
 		{
 			hanoi(1, 1, 3);
 		}
@@ -19,20 +19,20 @@ int main(int argc, char* argv[])
 		{
 			hanoi(1, 1, 2);
 		}
-		temp = a;
+		c = a;
 	}
 
 	return EXIT_SUCCESS;
 }
 
-void hanoi(int n, int from, int to)
+void hanoi(int n, int x, int y)
 {
 	if (n > 0)
 	{
-		int res = 6 - from - to;
-		hanoi(n - 1, from, res);
-		std::cout << 0 << " " << from << " " << to << std::endl;
-		hanoi(n - 1, res, to);
-		std::cout << n << " " << 6 - from - to << " " << 3 << std::endl;
+		int res = 6 - x - y;
+		hanoi(n - 1, x, res);
+		std::cout << 0 << " " << x << " " << y << std::endl;
+		hanoi(n - 1, res, y);
+		std::cout << n << " " << 6 - x - y << " " << 3 << std::endl;
 	}
 }
